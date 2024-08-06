af671e3ad1b5b49033da583aadff42f0330dc20e
theharism <chharis9999@gmail.com>
2024-08-07 00:58:35 +0500

    home modify

--- app/(app)/(home)/index.jsx
+++ app/(app)/(home)/index.jsx
@@ -1,7 +1,10 @@
 import {
   Image,
+  KeyboardAvoidingView,
+  Platform,
   Pressable,
   SafeAreaView,
+  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
@@ -11,86 +14,124 @@ import React from "react";
 import { theme } from "../../src/constants/theme";
 import { Link, useNavigation } from "expo-router";
 
-const Home = () => {
-  const Module = ({ text }) => {
-    return (
-      <Link
-        href="/(sales)"
-        style={{
-          backgroundColor: "orange",
-          margin: 10,
-          padding: 20,
-          height: "28%",
-          justifyContent: "flex-end",
-          borderRadius: 25,
-        }}
-        asChild
-      >
-        <Pressable>
-          <Text
-            style={{
-              color: theme.colors.secondary,
-              fontStyle: "italic",
-              fontWeight: "bold",
-            }}
-          >
-            {text}
-          </Text>
-        </Pressable>
-      </Link>
-    );
-  };
-
+const Module = ({ text, bg, fg }) => {
   return (
-    <SafeAreaView style={styles.backgroundStyle}>
-      <StatusBar
-        barStyle={"light-content"}
-        backgroundColor={theme.colors.background}
-      />
-
-      <View
-        style={{
-          justifyContent: "center",
-          paddingTop: 50,
-          paddingBottom: 10,
-          //   backgroundColor: "red",
-          paddingHorizontal: 10,
-          flexGrow: 1,
-        }}
-      >
+    <Link
+      href="/(sales)"
+      style={{
+        backgroundColor: "orange",
+        marginVertical: 25,
+        marginHorizontal: 10,
+        height: 150,
+        justifyContent: "flex-end",
+        borderRadius: 25,
+      }}
+      asChild
+    >
+      <Pressable style={{ flex: 1 }}>
         <Image
-          source={require("../../../assets/logo.png")}
-          style={{ alignSelf: "center" }}
+          source={bg}
+          style={{
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            borderRadius: 25,
+          }}
+          resizeMode="cover"
+        />
+
+        <Image
+          source={fg}
+          style={{
+            width: "40%",
+            height: "130%",
+            position: "absolute",
+            right: -5,
+            bottom: -20,
+            zIndex: 999,
+          }}
         />
 
         <Text
           style={{
-            textAlign: "center",
-            fontSize: 28,
-            fontWeight: "300",
-            marginTop: 5,
             color: theme.colors.secondary,
+            fontStyle: "italic",
+            fontWeight: "bold",
+            padding: 20,
           }}
         >
-          My Sales Coach
+          {text}
         </Text>
-        <Image
-          source={require("../../../assets/welcome.png")}
-          style={{ alignSelf: "center", marginTop: 20 }}
+      </Pressable>
+    </Link>
+  );
+};
+
+const Home = () => {
+  return (
+    <SafeAreaView style={styles.backgroundStyle}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+        style={{ flex: 1 }}
+        enabled
+      >
+        <StatusBar
+          barStyle={"light-content"}
+          backgroundColor={theme.colors.background}
         />
 
-        <View
-          style={{
-            // backgroundColor: "yellow",
-            flex: 1,
-            justifyContent: "flex-end",
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          keyboardShouldPersistTaps={"handled"}
+          contentContainerStyle={{
+            justifyContent: "center",
+            paddingTop: 50,
+            paddingBottom: 10,
+            paddingHorizontal: 10,
+            flexGrow: 1,
           }}
+          bounces={false}
         >
-          <Module text={"Track sales activity"} />
-          <Module text={"Ask my coach"} />
-          <Module text={"Watch masterclass"} />
-        </View>
-      </View>
+          <Image
+            source={require("../../../assets/logo.png")}
+            style={{ alignSelf: "center" }}
+          />
+
+          <Text
+            style={{
+              textAlign: "center",
+              fontSize: 28,
+              fontWeight: "300",
+              marginTop: 5,
+              color: theme.colors.secondary,
+            }}
+          >
+            My Sales Coach
+          </Text>
+          <Image
+            source={require("../../../assets/welcome.png")}
+            style={{ alignSelf: "center", marginTop: 20 }}
+          />
+
+          <View style={{ marginTop: 30 }}>
+            <Module
+              bg={require("../../../assets/1.png")}
+              fg={require("../../../assets/1a.png")}
+              text={"Track sales activity"}
+            />
+            <Module
+              bg={require("../../../assets/2.png")}
+              fg={require("../../../assets/2a.png")}
+              text={"Ask my coach"}
+            />
+            <Module
+              bg={require("../../../assets/3.png")}
+              fg={require("../../../assets/3a.png")}
+              text={"Watch masterclass"}
+            />
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
