0bb04df610e1dd3405ad70e651822614d88185eb
theharism <chharis9999@gmail.com>
2024-08-08 02:14:37 +0500

    tabs fix

--- app/(app)/(sales)/(tabs)/_layout.jsx
+++ app/(app)/(sales)/(tabs)/_layout.jsx
@@ -3,9 +3,10 @@ import { Tabs } from "expo-router";
 export default function TabLayout() {
   return (
     <Tabs screenOptions={{ headerShown: false, tabBarHideOnKeyboard: true }}>
-      <Tabs.Screen name="Daily Activity" />
+      <Tabs.Screen name="index" options={{ tabBarLabel: "Daily Activity" }} />
       <Tabs.Screen name="Activity Reports" />
       <Tabs.Screen name="Effectiveness Report" />
+      <Tabs.Screen name="Annual Progress" />
     </Tabs>
   );
 }
